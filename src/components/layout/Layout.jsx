71de3e35fb1e6152155629399090b7741b6f7971
DanielRochaOdo <daniel.rocha@odontoart.com>
2026-01-02 13:10:59 -0300

    implementa tema escuro

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,9 +4,9 @@ import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col lg:flex-row min-h-screen bg-gray-50">
+    <div className="flex flex-col lg:flex-row min-h-screen bg-background text-foreground">
       <Sidebar />
-      <main className="flex-1 overflow-auto">
+      <main className="flex-1 overflow-auto lg:pl-[var(--sidebar-width)] transition-[padding] duration-300">
         <div className="p-4 sm:p-6 lg:p-8">
           {children}
         </div>
